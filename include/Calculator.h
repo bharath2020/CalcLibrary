58e02bba918c6518787c3267927959298911360b
Bharath Booshan <bharath2020@gmail.com>
2014-07-26 13:08:37 -0700

    Initial Commit

---
+++ include/Calculator.h
@@ -0,0 +1,16 @@
+//
+//  Calculator.h
+//  Calculator
+//
+//  Created by Bharath Booshan on 7/26/14.
+//  Copyright (c) 2014 FeatherTouch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calculator : NSObject
+
+-(int)APlusBWholeSquare:(int)a  andB:(int)b;
+
+
+@end
